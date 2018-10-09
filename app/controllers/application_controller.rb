6de8f91a76b8566d714c40523e2346ea07a3eff4
hjiang00 <hjiang00@umail.ucsb.edu>
2018-10-09 08:21:24 +0000

    Homepage setting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  
+  def homepage
+    render html: "Welcome to HYHYH! -- A community to watch interesting videos together!"
+  end
+  
 end
