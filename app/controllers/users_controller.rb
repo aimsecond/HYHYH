0eab3e25a906f445c902bcbb6714fb196a3b8f2f
hjiang00 <hjiang00@umail.ucsb.edu>
2018-10-22 01:47:08 +0000

    Complete signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,25 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      flash[:success] = "Create or join a room to watch!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :password,
+                                   :password_confirmation)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_22_003242) do
+ActiveRecord::Schema.define(version: 2018_10_22_005656) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "email"
     t.integer "room_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
