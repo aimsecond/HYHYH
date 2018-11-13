bd9e81fd9eefe64edebc2092441d79709c5669d1
haowen zhang <aimsecond12@gmail.com>
2018-11-12 22:40:33 -0800

    tutorial, for deployment issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://hyhyh1.jm8qqevcmb.us-west-2.elasticbeanstalk.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://hyhyh1.jm8qqevcmb.us-west-2.elasticbeanstalk.com' ]
+  config.action_cable.allowed_request_origins = [ /(http|https):\/\/.*\.elasticbeanstalk\.com/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
