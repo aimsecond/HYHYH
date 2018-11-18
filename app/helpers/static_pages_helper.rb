1bdf9b483114525943cc5feab797894d844543b3
Lavden <1244361371@qq.com>
2018-11-17 17:42:05 -0800

    add validate url, remove mock url

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,17 @@
 module StaticPagesHelper
+
+    def convert_link(link)
+        if link.include? 'watch?v='
+            'https://www.youtube.com/embed/' + link.split('watch?v=')[1]
+        end 
+    end
+
+    def get_thumbnail(link)
+        'https://img.youtube.com/vi/' + link.split('watch?v=')[1] + '/default.jpg'
+    end
+
+    def get_title(link)
+        video = VideoInfo.new(link)
+        video.title
+    end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -2,5 +2,14 @@ class Room < ApplicationRecord
   validates :link_1, presence: true
   validates :room_name, presence: true, length: { maximum: 30 }
   validates :host_id, presence: true
+  validate :valid_url
   has_many :users
+end
+
+private 
+
+def valid_url
+  unless link_1.include? "watch?v="
+    errors.add(:link_1, "Wrong link format")
+  end
 end
