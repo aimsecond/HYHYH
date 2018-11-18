2c2f78a880ec49fc57d501078add3d8fd4c1bc8a
haowen zhang <aimsecond12@gmail.com>
2018-11-17 21:55:01 -0800

    add chatroom to message model

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,10 @@ ActiveRecord::Schema.define(version: 2018_11_06_073258) do
   create_table "messages", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
+    t.integer "room_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
