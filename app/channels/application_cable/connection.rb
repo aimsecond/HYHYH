a1ee77e083c54099e8f52473d5cc877dc706e8c1
haowen zhang <aimsecond12@gmail.com>
2018-11-19 11:41:02 -0800

    added cookie field for channel authentication

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -4,18 +4,16 @@ module ApplicationCable
 
     def connect
       self.current_user = find_verified_user
-      logger.add_tags "ActionCable", "User #{current_user.id}"
     end
 
     protected
-    def find_verified_user
-      # Assuming a successful authentication sets a signed cookie with the `user_id`
-      if verified_user = User.find_by(id: cookies.signed[:user_id])
-        verified_user
-      else
-        # Raises ActionCable::Connection::Authorization::UnauthorizedError
-        reject_unauthorized_connection
+      def find_verified_user
+        if current_user = User.find_by(id: cookies.signed[:user_id])
+          current_user
+        else
+          reject_unauthorized_connection
+        end
       end
-    end
+
   end
 end
