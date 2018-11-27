e2c126a642494793324b2d89cf0cd46ea5519570
hjiang00 <hjiang00@umail.ucsb.edu>
2018-11-27 21:33:59 +0000

    still implementing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_18_063148) do
+ActiveRecord::Schema.define(version: 2018_11_27_195253) do
 
   create_table "active_users", force: :cascade do |t|
     t.integer "room_id"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 2018_11_18_063148) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "playlists", force: :cascade do |t|
+    t.integer "room_id"
+    t.string "link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "title"
   end
 
   create_table "rooms", force: :cascade do |t|
