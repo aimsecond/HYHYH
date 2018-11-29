4a3f4a7318ad76d913edca003cfea6108ecee80a
hjiang00 <hjiang00@umail.ucsb.edu>
2018-11-29 22:55:44 +0000

    finish seeds for 500 users(host), 500 rooms, related playlist (2 links in total each 500 times), active user(1 for each room)

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,5 +16,22 @@ room_array = []
   room_array << Room.create!(host_id: user.id, room_name: user.username+"'s Room", link_1: "https://www.youtube.com/watch?v=g-N3s4sBlvs", link_2: "https://www.youtube.com/watch?v=kJQP7kiw5Fk")
 end
 
+playlist_array = []
+(1..500).each do |num|
+  if num == 1
+    playlist_array << Playlist.create!(link: "https://www.youtube.com/watch?v=g-N3s4sBlvs", title: "Load-testing a Rails app with Tsung on AWS Elastic Beanstalk", count:1 )
+    playlist_array << Playlist.create!(link: "https://www.youtube.com/watch?v=kJQP7kiw5Fk", title: "Luis Fonsi - Despacito ft. Daddy Yankee (Official Music Video)", count:1 )
+  else
+    Playlist.find_by_link("https://www.youtube.com/watch?v=g-N3s4sBlvs").increment!(:count)
+    Playlist.find_by_link("https://www.youtube.com/watch?v=kJQP7kiw5Fk").increment!(:count)
+  end
+end
+
+active_user_array = []
+(room_array).each do |room|
+  active_user_array << ActiveUser.create!(room_id: room.id, user_count: 1)
+end
+
+
 
 
