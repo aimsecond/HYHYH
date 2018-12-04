963c51cd4d22e6c730da84a654621a118afa240e
hjiang00 <hjiang00@umail.ucsb.edu>
2018-12-04 22:13:01 +0000

    add caching in lobby and playlists

--- app/helpers/playlists_helper.rb
+++ app/helpers/playlists_helper.rb
@@ -1,2 +1,5 @@
 module PlaylistsHelper
+  def cache_key_for_playlists(video)
+    "playlists/#{video.id}/#{video.updated_at}/#{video.count}"
+  end
 end
--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,2 +1,5 @@
 module RoomsHelper
+  def cache_key_for_rooms(room)
+    "rooms/#{room.id}/#{room.updated_at}/#{room.users.size}"
+  end
 end
