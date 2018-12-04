023ccbe57b697d23d3cead6419e17f67f1388993
hjiang00 <hjiang00@umail.ucsb.edu>
2018-12-04 22:42:29 +0000

    revise seed to 1000 rooms

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,23 +7,23 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 user_array = []                                                         
-(1..250).each do |num|                                                       
+(1..1000).each do |num|                                                       
   user_array << User.create!(username: "User"+num.to_s, password: "password"+num.to_s, room_id: nil)                 
 end 
 
 room_array = []
 @user_offset = user_array.first.id
 (user_array).each do |user|
-  if user.id >= @user_offset && user.id <= (125 + @user_offset)
+  if user.id >= @user_offset && user.id <= (500 + @user_offset)
     randName = SecureRandom.hex(5)
     @room = Room.create!(host_id: user.id, room_name: user.username+"'s Room", link_1: "https://www.youtube.com/watch?v=" + randName.to_s + user.id.to_s, link_2: "https://www.youtube.com/watch?v=" + randName.to_s + user.id.to_s)
     room_array << @room
     User.find(user.id).update_attribute('room_id', @room.id)
-  elsif user.id > (125 + @user_offset) && user.id <= (240 + @user_offset)
+  elsif user.id > (500 + @user_offset) && user.id <= (800 + @user_offset)
     @room = Room.create!(host_id: user.id, room_name: user.username+"'s Room", link_1: "https://www.youtube.com/watch?v=kJQP7kiw5Fk")
     room_array << @room
     User.find(user.id).update_attribute('room_id', @room.id)
-  elsif user.id > (240 + @user_offset) && user.id <= (250 + @user_offset)
+  elsif user.id > (800 + @user_offset) && user.id <= (1000 + @user_offset)
     @room = Room.create!(host_id: user.id, room_name: user.username+"'s Room", link_1: "https://www.youtube.com/watch?v=g-N3s4sBlvs")
     room_array << @room
     User.find(user.id).update_attribute('room_id', @room.id)
@@ -31,9 +31,9 @@ room_array = []
 end
 @room_offset = room_array.first.id
 user_array_join = []
-(251..500).each do |num|
+(1001..2000).each do |num|
   prng = Random.new
-  room = prng.rand(0..249)
+  room = prng.rand(0..250)
   user_array_join << User.create!(username: "User"+num.to_s, password: "password"+num.to_s, room_id: @room_offset + room)
 end
 
@@ -41,12 +41,12 @@ end
 Playlist.create!(link: "https://www.youtube.com/watch?v=kJQP7kiw5Fk", title: "Most Popular link", count:1 )
 Playlist.create!(link: "https://www.youtube.com/watch?v=g-N3s4sBlvs", title: "Second Popular link", count:1 )
 (room_array).each do |room|
-  if room.id >= @room_offset && room.id <= (125 + @room_offset)
+  if room.id >= @room_offset && room.id <= (500 + @room_offset)
     Playlist.create!(link: room.link_1, title: "testlink in " + room.id.to_s, count:1 )
     Playlist.create!(link: room.link_2, title: "testlink2 in " + room.id.to_s, count:1 )
-  elsif room.id > (125 + @room_offset) && room.id <= (240 + @room_offset)
+  elsif room.id > (500 + @room_offset) && room.id <= (800 + @room_offset)
     Playlist.find_by_link("https://www.youtube.com/watch?v=kJQP7kiw5Fk").increment!(:count)
-  elsif room.id > (240 + @room_offset) && room.id <= (250 + @room_offset)
+  elsif room.id > (800 + @room_offset) && room.id <= (1000 + @room_offset)
     Playlist.find_by_link("https://www.youtube.com/watch?v=g-N3s4sBlvs").increment!(:count)
   end
 end
@@ -54,11 +54,10 @@ end
 
 (room_array).each do |room|
   ActiveUser.create!(room_id: room.id, user_count: 1)
+  Message.create!(room_id: room.id, user_id: room.host_id, content: "my room is my house")
+  Message.create!(room_id: room.id, user_id: room.host_id, content: "I am the king of my room and I control the paly and pause")
 end
 (user_array_join).each do |user_join|
   ActiveUser.find_by_room_id(user_join.room_id).increment!(:user_count)
 end
-
-
-
 
